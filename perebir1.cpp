7340f5c649e4f5b513e2a8265928fdfbeee25eaf
bodyanuch <bodyanuch_ne@ukr.net>
2016-04-05 10:33:58 +0300

    працюює

--- perebir1.cpp
+++ perebir1.cpp
@@ -4,55 +4,26 @@
 using namespace std;
 /*count how many tickets are lucky tickets for bus. Ticket is lucky when first and last digits of number of ticket is equal*/
 
-int x,y,z = 0;
+int x = 0, y = 0, z = 0;
 
-int z_count() 
+int main() 
 {
 do {
+do {
+	if (z < 9) {
 z++;
-} while (z<10); 
-if (x == z){
-	cout << "Lucky ticket" << x << y << z << endl;
 } else {
-	return 0;
-}}
-
-int y_count() 
-{
-do {
-if (z == 9) {
-y++;
-} else {
-	return 0;
+	y++;
+	z = 0;
 }
-z_count();
-} while (y<10);
-if (x == z){
-	cout << "Lucky ticket" << x << y << z << endl;
-} else {
-	return 0;
+if (x == z) {
+	cout << "Lucky ticket:" << x << y << z << endl;
+} else {}
+} while (y < 10);
+	if (y > 8) {
+		x++;
+		y = 0;
 	}
-}
-
-int x_count() 
-{
-do {
-	if (y == 9) {
-x++;
-} else {
-	return 0;
-}
-y_count();
-} while (x<10);
-if (x == z){
-	cout << "Lucky ticket" << x << y << z << endl;
-} else {
-	return 0;
-	}
-}
-
-int main()
-{
-	x_count();
-	return 0;
+	else {}
+} while (x < 10);
 }
